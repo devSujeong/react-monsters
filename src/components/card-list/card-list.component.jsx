fc4b38d0c18b7c42a1c26621f73dd6620563736b
crystal - Kim Sujeong <sjkim@2ndsyndrome.com>
2021-01-16 15:19:20 +0900

    register

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { Card } from '../card/card.component';
+
+import './card-list.styles.css';
+
+export function CardList(props) {
+  return (
+      <div className="card-list">
+        {props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
+        ))}
+      </div>
+  );
+}
